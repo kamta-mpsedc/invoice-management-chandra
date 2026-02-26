91c54845d785667c8e254df213f9eebe0123218b
kamta-mpsedc <kamta.mpsedc@gmail.com>
2026-02-26 07:58:52 +0530

    Delete chandra-code/Views directory

--- chandra-code/Views/login.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="<?php echo base_url('authlogin') ?>">
-    <input type="email" name="email" placeholder="Email"><br><br>
-    <input type="password" name="password" placeholder="Password"><br><br>
-    <button type="submit">Login</button>
-</form>
